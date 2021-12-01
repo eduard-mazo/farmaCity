1bde10587ebeac98c0bc13bfe449ed423fd26f59
Eduard Mazo <eduard.mazo@gmail.com>
2021-11-30 23:24:21 -0500

    Adding xml decode script

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ const fs = require('fs');
 const xml2json = require('xml2json');
 const filepath = __dirname + '\\facturas\\';
 
-async function test(filename) {
-    console.log(filename);
+async function parseXml(filename) {
+    console.log('Parsing ' + filename);
+    var json;
+    var invoice;
+    var items;
+    fs.readFile(filepath + filename, (err, data) => {
+        json = JSON.parse(xml2json.toJson(data));
+        invoice = JSON.parse(xml2json.toJson(json.AttachedDocument["cac:Attachment"]["cac:ExternalReference"]["cbc:Description"]))
+        items = invoice.Invoice["cac:InvoiceLine"]
+        items.forEach(item => {
+            decodeItem(item)
+        })
+    })
 }
 
 function readingFiles() {
     fs.readdir(filepath, (err, files) => {
         files.forEach(file => {
             if (file.indexOf('.xml') > 0) {
-                test(file);
+                parseXml(file);
             }
         })
     })
+}
+
+function decodeItem(item) {
+    console.log('DESCRIPCIÓN --> ' + item["cac:Item"]["cbc:Description"]);
+    console.log('PRECIO --> ' + item["cac:Price"]["cbc:PriceAmount"].$t);
 }
 
 readingFiles();
