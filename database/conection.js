af5d30561f0e150b280645ff18695e4e1629a4db
Eduard Mazo <eduard.mazo@gmail.com>
2022-03-27 18:47:42 -0500

    Fixing security issues

--- database/conection.js
+++ database/conection.js
@@ -2,24 +2,22 @@ const mongoose = require('mongoose');
 const Admin = mongoose.mongo.Admin;
 
 async function main() {
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-  const uri =
-    'mongodb+srv://EASC_01:0KzQlxBvEk8yDmLV@thundersandbox-v0ydj.mongodb.net/luna?retryWrites=true&w=majority';
+	/**
+	 * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+	 * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+	 */
+	const uri = process.env.MONGO_URI;
+	try {
+		// Connect to the MongoDB cluster
+		await mongoose.connect(uri, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 
-  try {
-    // Connect to the MongoDB cluster
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log('MongoDB connected!!');
-  } catch (e) {
-    console.error(e);
-  }
+		console.log('MongoDB connected!!');
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 module.exports.main = main;
